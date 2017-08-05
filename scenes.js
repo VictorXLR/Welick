3a2ca1bdf447e787dafb7322ace7806505454ff4
VictorXLR <VictorXLR@noreply.github.com>
2017-08-04 22:21:18 -0300

    new

---
+++ scenes.js
@@ -0,0 +1,17 @@
+var start ={
+
+	text:"Hi, Welcome to Welick, The Aim of this Game to leave [room](start)"
+
+}
+loadScene(start)
+
+
+var room={
+	text: "You wake up in a room with 3 doors [kitchen](kitchen),[wardrobe](wardrobe), [toilet](Toilet)"
+
+}
+
+var toilet ={
+	text: "You go into the toilet, you see a [cup](cup), some [towels](towels) and a foggy [mirror](mirror)  "
+}
+
